58bdb1c02dc110b2983186ebe1ecbe1b1c7311b4
“Nidhu23” <nidhunanik@gmail.com>
2020-06-15 19:48:38 +0530

    Added: Method To Search A Person

--- Addressbook.java
+++ Addressbook.java
@@ -20,32 +20,41 @@ public class Addressbook {
 			System.out.println("Enter your choice");
 			ch=sc.nextInt();
 			switch(ch) {
-			case 1: do {
-					System.out.println("Enter first name");
-					firstName=sc.next();
-					System.out.println("Enter last name");
-					lastName=sc.next();
-					System.out.println("Enter street");
-					street=sc.next();
-					System.out.println("Enter city");
-					city=sc.next();
-					System.out.println("Enter state");
-					state=sc.next();
-					System.out.println("Enter zipcode");
-					zipCode=sc.nextInt();
-					System.out.println("Enter phone number");
-					phoneNum=sc.nextLong();
-					Persondetails person=new Persondetails(firstName,lastName,street,city,state,zipCode,phoneNum);
-					address.add(person.getFirstName(),person);
-					System.out.println("Enter 1 to add more people, 0 to stop");
-					ch=sc.nextInt();
-			}while(ch!=0);break;
-		}
+					case 1:
+							do {
+								System.out.println("Enter first name");
+								firstName=sc.next();
+								System.out.println("Enter last name");
+								lastName=sc.next();
+								System.out.println("Enter street");
+								street=sc.next();
+								System.out.println("Enter city");
+								city=sc.next();
+								System.out.println("Enter state");
+								state=sc.next();
+								System.out.println("Enter zipcode");
+								zipCode=sc.nextInt();
+								System.out.println("Enter phone number");
+								phoneNum=sc.nextLong();
+								Persondetails person=new Persondetails(firstName,lastName,street,city,state,zipCode,phoneNum);
+								address.add(person.getFirstName(),person);
+								System.out.println("Enter 1 to add more people, 0 to stop");
+								ch=sc.nextInt();
+							}while(ch!=0);break;
+					case 2:
+							System.out.println("Enter the name of the person to search");
+							firstName=sc.next();
+							search(firstName); break;
+			}
 			System.out.println("Enter 0 to quit, 1 to go to main menu");
 			ch=sc.nextInt();
 		}while(ch!=0);
 	}
 	public void add(String nameKey,Persondetails P) {
 		personMap.put(nameKey, P);
+	}
+	public void search(String name) {
+		Persondetails p=personMap.get(name);
+		System.out.println(name+"'s details are:\n"+"Last Name: "+p.getLastName()+"\nAddress: "+p.getStreet()+", "+p.getCity()+","+p.getState()+", "+"\nPhone Num: "+p.getPhoneNum());
 	}
 }
